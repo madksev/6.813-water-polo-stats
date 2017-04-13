707685a9989d19b72b76b2b2d24fba54fa2ed297
madksev <madksev@mit.edu>
2017-04-13 00:18:38 -0400

    basic view for pool and active players

---
+++ index.js
@@ -0,0 +1,49 @@
+var GOALIE_LEFT = true;
+var STATISTIC_TYPES = ['goal', 'shot', 'assist', 'block', 'steal', 'turnover', 'ejection received', 'ejection drawn'];
+var game = new Game();
+var newPlayers = [['Lily', 'Chen', 1], ['Beth', 'Gates', 3], ['Abby', 'Wilson', 4], ['Jane', 'Lee', 5], ['Grace','Jones', 7], ['Alex', 'Lange', 10], ['Danielle', 'Flowers', 11], ['Sarah', 'Hunt', 12], ['Marie', 'Knowles', 14], ['Claire', 'Davis', 15], ['Cindy', 'Xiang', 16]];
+for (i=0; i<newPlayers.length; i++) {
+  game.addPlayer(newPlayers[i][0], newPlayers[i][1], newPlayers[i][2]);
+}
+
+//make 6 players active and 1 player goalie
+for (i=0; i<7; i++) {
+  var inactivePlayers = game.getPlayersWithState('BENCH');
+  if (i==0) {
+    game.setPlayerState(inactivePlayers[0], 'BENCH', 'GOALKEEPER');
+  } else {
+    game.setPlayerState(inactivePlayers[0], 'BENCH', 'ACTIVE');
+  }
+}
+
+if (!game.checkTeamIsValid) {
+  alert('Error setting up game');
+}
+
+
+$(document).ready(function() {
+
+  var goalie = game.getPlayersWithState('GOALKEEPER')[0];
+  $('#playersContainer').append('<button type="button" class="btn btn-outline-success player-btn"><span>'+
+    goalie.capNumber+'</span><br><span class="player-name">'+
+    goalie.lastName+'</span></button>');
+
+  var activePlayers = game.getPlayersWithState('ACTIVE');
+  for (i=0; i<activePlayers.length; i++) {
+    $('#playersContainer').append('<button type="button" class="btn btn-outline-success player-btn"><span>'+
+      activePlayers[i].capNumber+'</span><br><span class="player-name">'+
+      activePlayers[i].lastName+'</span></button>');
+  }
+});
+
+$(document).on('click', '#switchSidesBtn', function(evt) {
+  if (GOALIE_LEFT) {
+    $('#leftGoalieContainer').empty();
+    $('#rightGoalieContainer').append('Your Goalie');
+    GOALIE_LEFT = false;
+  } else {
+    $('#rightGoalieContainer').empty();
+    $('#leftGoalieContainer').append('Your Goalie');
+    GOALIE_LEFT = true;
+  }
+});
