561eef7acf8804e2f9b80dcac68e340b3a083f52
Parker Greene <pwgreene@mit.edu>
2017-04-27 21:24:56 -0400

    added undo functionality to log

--- log.js
+++ log.js
@@ -40,12 +40,74 @@ function createLogEntry(player, action, time) {
   button.innerHTML = "&#10006"
   entry.appendChild(button);
   $(button).on('click', function(e, info) {
-    deleteLogEntry(entry);
+    XLogEntry(entry);
   });
+  $(entry).attr("toDelete", false);
   return entry;
+}
+
+function XLogEntry(entry) {
+  //pressing the "X" button on a log entry, not same as deleting
+  var cover = document.createElement("div");
+  cover.className = "logEntry-cover";
+  var undoDiv = document.createElement("div");
+  undoDiv.innerHTML = "UNDO";
+  undoDiv.className = "logEntry-undo";
+  $(undoDiv).on("click", function(e, info) {
+    entry.removeChild(cover);
+    $(entry).attr("toDelete", false);
+    checkForEntriesToRemove();
+  });
+  
+  var removeDiv = document.createElement("div");
+  removeDiv.innerHTML = "REMOVE";
+  removeDiv.className = "logEntry-undo";
+  $(removeDiv).on("click", function(e, info) {
+    deleteLogEntry(entry);
+    checkForEntriesToRemove();
+  });
+  
+  cover.appendChild(undoDiv);
+  cover.appendChild(removeDiv);
+  entry.appendChild(cover);
+  $(entry).attr("toDelete", true);
+  displayRemoveAll("block");
 }
 
 function deleteLogEntry(entry) {
   log = document.getElementById("logTable");
   log.removeChild(entry);
 }
+
+function displayRemoveAll(displayType) {
+  btn = document.getElementById("log-removeAllBtn");
+  console.log(displayType);
+  btn.style.display = displayType;
+}
+
+function checkForEntriesToRemove() {
+  var childrenToDelete = [];
+  log = document.getElementById("logTable");
+  for (var i = 0; i < log.children.length; i++) {
+    if ($(log.children[i]).attr("toDelete") == "true") {
+      childrenToDelete.push(log.children[i]);
+    }
+  }
+  console.log(childrenToDelete);
+  if (childrenToDelete.length == 0) {
+    displayRemoveAll("none");
+  } else {
+    displayRemoveAll("block");
+  }
+  return childrenToDelete;
+}
+
+function removeAll() {
+  log = document.getElementById("logTable");
+  var childrenToDelete = checkForEntriesToRemove();
+  for (var i = 0; i < childrenToDelete.length; i++) {
+    log.removeChild(childrenToDelete[i]);
+  }
+  displayRemoveAll("none");
+}
+  
